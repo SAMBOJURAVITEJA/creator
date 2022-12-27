0ccda2f757c754e3fab4681bd2ff8cb97867faa8
SAMBOJURAVITEJA <ravitejasamboju@gmail.com>
2022-12-27 19:10:03 +0000

    message

---
+++ fine/ravi.mjs
@@ -0,0 +1,7 @@
+let a = function (k) {
+  return k ** k;
+};
+let value = 3;
+
+export default a;
+export default value;
---
+++ time/chelli.mjs
@@ -0,0 +1,5 @@
+let b = 3;
+import a from "../fine/ravi.mjs";
+import value from "../fine/ravi.mjs";
+console.log(value);
+console.log(a(3));
